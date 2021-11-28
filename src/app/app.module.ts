049907f0089595ed2ad4638526ebbacd54112989
Ludéric Freymann <luderic.freymann@etu.unistra.fr>
2021-11-28 23:23:15 +0100

    TP2 finished

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TetiereComponent } from './tetiere/tetiere.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { InputErrorDirective } from './input-error.directive';
+import { ResumeComponent } from './resume/resume.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TetiereComponent,
+    FooterComponent,
+    FormulaireComponent,
+    InputErrorDirective,
+    ResumeComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
